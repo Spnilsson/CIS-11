ee66176fae83e82f3822ed871b17b4a7a89f341e
spnilsson <spnilsson@hotmail.com>
2018-05-23 11:30:40 -0700

    assignment 11 complete

--- Assignments/Assignment 11/Project64_VS2017/main.asm
+++ Assignments/Assignment 11/Project64_VS2017/main.asm
@@ -1,14 +1,49 @@
-
-
 ExitProcess proto
+WriteInt64 proto
+Crlf proto
 
 .data
-number DWORD -20
+myArray QWORD 1,2,3,4,5,6,7,8
 
 .code
 main proc
-	mov eax,number
-   call ExitProcess
+
+	;print array
+	mov rcx, LENGTHOF myArray
+	mov rsi, 0
+	PRINTLOOP:
+		mov rax, myArray[rsi * 8]		
+		call WriteInt64
+		inc rsi
+	LOOP PRINTLOOP
+	call Crlf
+
+	mov rcx, LENGTHOF myArray / 2
+	mov rsi,0
+	mov rdi,1
+	SWAPPER:
+		mov r9,myArray[rsi * 8]
+		mov r10,myArray[rdi * 8]
+		mov myArray[rsi * 8],r10
+		mov myArray[rdi * 8],r9
+
+		add rsi,2
+		add rdi,2
+	LOOP SWAPPER
+
+	;print array
+	mov rcx, LENGTHOF myArray
+	mov rsi, 0
+	PRINTLOOP2:
+		mov rax, myArray[rsi * 8]		
+		call WriteInt64
+		inc rsi
+	LOOP PRINTLOOP2
+	call Crlf
+
+
+
+	call ExitProcess
 
 main endp
 end
--- Assignments/Assignment 8/Project64_VS2017/main.asm
+++ Assignments/Assignment 8/Project64_VS2017/main.asm
@@ -1,14 +1,50 @@
-
-
 ExitProcess proto
+WriteInt64 proto
+Crlf proto
 
 .data
-number DWORD -20
+decCon1 DWORD  11111111111111111111111111111110b
+decCon2 SDWORD 11111111111111111111111111111110b
+decCon3 DWORD  00000000000000000000000011111110b
+decCon4 SDWORD 00000000000000000000000011111110b
 
 .code
 main proc
-	mov eax,number
-   call ExitProcess
+	mov rcx,32
+	CONVER1:
+		
+	loop CONVER1
 
+	call WriteInt64		;prints the converted number, used after each loop
+	call Crlf
+	
+	mov rax,0	;reset rax
+	mov rcx,32
+	CONVER2:
+		
+	loop CONVER2
+
+	call WriteInt64		;prints the converted number, used after each loop
+	call Crlf
+
+	mov rax,0
+	mov rcx,32
+	CONVER3:
+		
+	loop CONVER3
+
+	call WriteInt64		;prints the converted number, used after each loop
+	call Crlf
+
+	mov rax,0
+	mov rcx,32
+	CONVER4:
+		
+	loop COnVER4
+
+	call WriteInt64		;prints the converted number, used after each loop
+	call Crlf
+
+	call ExitProcess
 main endp
 end
