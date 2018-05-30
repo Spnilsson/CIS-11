6247ba360daa4efd0f8efad2ad30555083f48200
spnilsson <spnilsson@hotmail.com>
2018-05-30 11:17:49 -0700

    assignment 6 update

--- Assignments/Assignment 6/question 10/Project64_VS2017/main.asm
+++ Assignments/Assignment 6/question 10/Project64_VS2017/main.asm
@@ -5,30 +5,40 @@ WriteInt64 proto
 Crlf proto
 
 .data
+fibonacci QWORD 47 DUP(?)
 
 
 .code
 main proc
+	mov rsi, OFFSET fibonacci
 	mov rcx,47
 	mov rax,0
-	mov rbx,1
-	mov r8,0
-	mov r9,0
-	
-L1:
-	mov r8,r9
-	add r8,rbx
-	mov rax,r8
-
 	call WriteInt64
 	call Crlf
 
-	mov r9,rbx
-	mov rbx,r8
+	mov [rsi],rax
+	add rsi, TYPE fibonacci
 
+	mov rax,1
+	call WriteInt64
+	call Crlf
+	
+	mov [rsi],rax
+	add rsi, TYPE fibonacci
 
+L1:
+	mov rdx, [rsi-16]
+	mov rbx, [rsi-8]
+
+	mov rax, rdx
+	add rax, rbx
+	mov [rsi],rax
+	add rsi, TYPE fibonacci
+	call WriteInt64
+	call Crlf
+	
 	loop L1
-   call ExitProcess
+	call ExitProcess
 
 main endp
 end
